71c35661f1f03162a105d18ccb81ca7d29940f89
Giorgi Lomidze <98590694+geo318@users.noreply.github.com>
2022-03-11 16:20:54 +0400

    next fixes, submit page

--- src/submits.js
+++ src/submits.js
@@ -20,8 +20,6 @@ function Submitted() {
     fetch("https://bootcamp-2022.devtest.ge/api/skills")
       .then((response) => response.json())
       .then((data) => setData1(data));
-
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
 
   const looper = (x, propToFind, propLookUp, returnProp) => {
@@ -33,137 +31,161 @@ function Submitted() {
   };
 
   return data.map((e, i) => (
-    <div>
-      {i + 1}
-      {console.log(data)}
+    <div key={i} className="outer-wrap">
       <div>
-        <div>
-          <h3>Personal Information</h3>
+        <div className="list">{i + 1}</div>
+        <div className="content-wrap">
           <div>
-            <p>
-              <div>First Name</div>
-              <div>{e.first_name}</div>
-            </p>
-            <p>
-              <div>Last Name</div>
-              <div>{e.last_name}</div>
-            </p>
-            <p>
-              <div>E Mail</div>
-              <div>{e.email}</div>
-            </p>
-            <p>
-              <div>Phone</div>
-              <div>{e.first_name}</div>
-            </p>
-          </div>
-        </div>
-        <div>
-          <h3>Skillset</h3>
-          {e["skills"].map((e, i) => (
             <div>
-              <p>
-                <div>{looper(data1, "id", e["id"], "title")}</div>
-                <div>Years of Experience: {e.experience}</div>
-              </p>
+              <h3>Personal Information</h3>
+              <div>
+                <p>
+                  <span>First Name</span>
+                  <span key={`f${i}`}>{e.first_name}</span>
+                </p>
+                <p>
+                  <span>Last Name</span>
+                  <span key={`l${i}`}>{e.last_name}</span>
+                </p>
+                <p>
+                  <span>E Mail</span>
+                  <span key={`e${i}`}>{e.email}</span>
+                </p>
+                <p>
+                  <span>Phone</span>
+                  <span key={`p${i}`}>{e.phone}</span>
+                </p>
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-      <div>
-        <h2>Covid Stuff</h2>
-        <div>
-          <p>how would you prefer to work?</p>
-          <div>
-            <input
-              type="radio"
-              id="office"
-              name="work_preference"
-              checked={e.work_preference === "From_Sairme_Office"}
-            />
-            <label htmlFor="office">From Sairme Office</label>
+            <div>
+              <h3>Skillset</h3>
+              {e["skills"].map((e, i) => (
+                <div key={i}>
+                  <p>
+                    <span key={`t${i}`}>
+                      {looper(data1, "id", e["id"], "title")}
+                    </span>
+                    <span key={`x${i}`}>
+                      Years of Experience: {e.experience}
+                    </span>
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
           <div>
-            <input
-              type="radio"
-              id="Home"
-              name="work_preference"
-              checked={e.work_preference === "From_Home"}
-            />
-            <label htmlFor="home">From Home</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Hybrid"
-              name="work_preference"
-              checked={e.work_preference === "Hybrid"}
-            />
-            <label htmlFor="Hybrid">Hybrid</label>
-          </div>
-        </div>
-        <div>
-          <p>Did you contact covid 19? :(</p>
-          <div>
-            <input
-              type="radio"
-              id="yes"
-              name="had_covid"
-              value="true"
-              checked={e.had_covid === "true"}
-            />
-            <label htmlFor="yesC">Yes</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="no"
-              name="had_covid"
-              value={e.had_covid}
-              checked={e.had_covid === "false"}
-            />
-            <label htmlFor="noC">No</label>
-          </div>
-          <div>
-            <p>When?</p>
-            <input
-              type="date"
-              id="dateC"
-              name="had_covid_at"
-              value={e.had_covid_at}
-            />
-          </div>
-        </div>
-        <div>
-          <p>Have you been vaccinated?</p>
-          <div>
-            <input
-              type="radio"
-              id="yesV"
-              name="vaccinated"
-              value="true"
-              checked={e.vaccinated === "true"}
-            />
-            <label htmlFor="yesV">yes</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="noV"
-              name="vaccinated"
-              value="false"
-              checked={e.vaccinated === "false"}
-            />
-            <label htmlFor="noV">no</label>
-          </div>
-          <div>
-            <p>When did you get your last covid vaccine?</p>
-            <input
-              type="date"
-              id="dateV"
-              name="vaccinated_at"
-              value={e.vaccinated_at}
-            />
+            <div>
+              <h3>Covid Stuff</h3>
+              <div>
+                <p>how would you prefer to work?</p>
+                <div>
+                  <input
+                    key={`Of${i}`}
+                    type="radio"
+                    id="office"
+                    name="work_preference"
+                    checked={e.work_preference === "from_sairme_office"}
+                    readOnly
+                  />
+                  <label htmlFor="office">From Sairme Office</label>
+                </div>
+                <div>
+                  <input
+                    key={`Ho${i}`}
+                    type="radio"
+                    id="Home"
+                    name="work_preference"
+                    checked={e.work_preference === "from_home"}
+                    readOnly
+                  />
+                  <label htmlFor="home">From Home</label>
+                </div>
+                <div>
+                  <input
+                    key={`Hy${i}`}
+                    type="radio"
+                    id="Hybrid"
+                    name="work_preference"
+                    checked={e.work_preference === "hybrid"}
+                    readOnly
+                  />
+                  <label htmlFor="Hybrid">Hybrid</label>
+                </div>
+              </div>
+              <div>
+                <p>Did you contact covid 19? :(</p>
+                <div>
+                  <input
+                    key={`CT${i}`}
+                    type="radio"
+                    id="yes"
+                    name="had_covid"
+                    checked={e.had_covid}
+                    readOnly
+                  />
+                  <label htmlFor="yesC">Yes</label>
+                </div>
+                <div>
+                  <input
+                    key={`CF${i}`}
+                    type="radio"
+                    id="no"
+                    name="had_covid"
+                    checked={e.had_covid === false}
+                    readOnly
+                  />
+                  <label htmlFor="noC">No</label>
+                </div>
+                <div>
+                  <p>When?</p>
+                  <input
+                    key={`DC${i}`}
+                    type="date"
+                    id="dateC"
+                    name="had_covid_at"
+                    value={e.had_covid_at}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div>
+                <p>Have you been vaccinated?</p>
+                <div>
+                  <input
+                    key={`Vy${i}`}
+                    type="radio"
+                    id="yesV"
+                    name="vaccinated"
+                    checked={e.vaccinated === true}
+                    readOnly
+                  />
+                  <label htmlFor="yesV">yes</label>
+                </div>
+                <div>
+                  <input
+                    key={`Vn${i}`}
+                    type="radio"
+                    id="noV"
+                    name="vaccinated"
+                    value="false"
+                    checked={e.vaccinated === "false"}
+                    readOnly
+                  />
+                  <label htmlFor="noV">no</label>
+                </div>
+                <div>
+                  <p>When did you get your last covid vaccine?</p>
+                  <input
+                    key={`VD${i}`}
+                    type="date"
+                    id="dateV"
+                    name="vaccinated_at"
+                    value={e.vaccinated_at}
+                    readOnly
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
